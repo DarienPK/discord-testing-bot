ec59f3fb2388c9baea2df7b736a7ff8e12bb4cb2
Darien P <darien.x.perkins@gmail.com>
2026-05-26 00:51:46 -0500

    speak troubleshooting: does register work without turning off bot?

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
             {
               type: MessageComponentTypes.TEXT_DISPLAY,
               // Fetches a random emoji to send from a helper function
-              content: `hello world, the world continues spinning`
+              content: `hello world, the world unendingly revolves`
             }
           ]
         },
