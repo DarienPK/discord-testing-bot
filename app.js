4a7d383aaaa08a8f6308b3d87e8d8e947957393a
Darien P <darien.x.perkins@gmail.com>
2026-05-30 19:05:41 -0500

    somehow restarting this computer worked lol

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
                 {
                   type: MessageComponentTypes.TEXT_DISPLAY,
                   //
-                  content: `hello world, i am a new test 2`
+                  content: `hello world, i am a new test 3`
                 }
               ]
             },
